4bba0d1f61c8db8ecb2cb0880e40477bd35c8cac
Hoby <Hoby@iMac-Kaizer-9.local>
2022-04-24 21:53:31 +0200

    Updates

---
+++ precache-manifest.a32f124d748613af0035ef6c50c65442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364cf919f8cf8f1315c535145e95724b",
+    "url": "/alyra-voting-dapp/index.html"
+  },
+  {
+    "revision": "aec2f7c74601045f0117",
+    "url": "/alyra-voting-dapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "635a8375c262452cec2a",
+    "url": "/alyra-voting-dapp/static/js/2.0df738b0.chunk.js"
+  },
+  {
+    "revision": "aec2f7c74601045f0117",
+    "url": "/alyra-voting-dapp/static/js/main.2119179b.chunk.js"
+  },
+  {
+    "revision": "f7fb7be1b9bfc5d80104",
+    "url": "/alyra-voting-dapp/static/js/runtime-main.9e81360e.js"
+  }
+]);
